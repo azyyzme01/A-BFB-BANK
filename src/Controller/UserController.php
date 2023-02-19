95b9071b47d868aa01fa34d6bd7e5885154797ba
azyyzme01 <ayed.mohamedaziz@esprit.tn>
2023-02-19 17:46:38 +0100

    fullcrud

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    #[Route('/afficher', name: 'afficher_front', methods: ['GET'])]
+    public function index(UserRepository $userRepository): Response
     {
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'users' => $userRepository->findAll(),
         ]);
     }
 }
